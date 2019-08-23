21f44f15719e9e0c98c1cdf5723493a9c05e6702
adeoye adela <adeoye.adela@gmail.com>
2019-08-23 18:08:03 +0100

    Step 2 and Step 3 of functions.js DONE

--- challenges/functions.js
+++ challenges/functions.js
@@ -17,12 +17,22 @@ function consume(a, b, cb) {
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-
-
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+const greeting = (first_name, last_name) => {
+        return (`Hello ${first_name} ${last_name}, nice to meet you`);
+    }
+    /* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
