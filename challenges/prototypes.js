d481554b0ab4173bc52ec749f8c6fc040eb57482
adeoye adela <adeoye.adela@gmail.com>
2019-08-23 18:35:34 +0100

    Step 4 and testing  of prototypes.js DONE

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -26,7 +26,7 @@ CuboidMaker.prototype.volume = function() { return (this.length * this.width * t
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-CuboidMaker.prototype.surfaceArea = function() { return (this.length * this.width + this.length * this.height + this.width * this.height) }
+CuboidMaker.prototype.surfaceArea = function() { return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height)) }
 
 
 
@@ -34,22 +34,12 @@ CuboidMaker.prototype.surfaceArea = function() { return (this.length * this.widt
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-// onst mage = new Humanoid({
-//   createdAt: new Date(),
-//   dimensions: {
-//       length: 2,
-//       width: 1,
-//       height: 1,
-//   },
-//   healthPoints: 5,
-//   name: 'Bruce',
-//   team: 'Mage Guild',
-//   weapons: [
-//       'Staff of Shamalama',
-//   ],
-//   language: 'Common Tongue',
-// });
+const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5,
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
