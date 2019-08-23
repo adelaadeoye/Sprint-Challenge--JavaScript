45e832d84e1e445b2fd782c05045a5309ed06129
adeoye adela <adeoye.adela@gmail.com>
2019-08-23 17:18:27 +0100

    Request 3: .filter() of ADVANCED Array Methods GRANTED

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -141,13 +141,16 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 const lowCaseAnimalNames = zooAnimals.map(currentValue => {
     return (currentValue.animal_name.toLowerCase());
 })
-console.log(`\n${JSON.stringify(lowCaseAnimalNames)}`)
-    /* Request 3: .filter() 
+console.log(`\n${JSON.stringify(lowCaseAnimalNames)}\n`)
 
-    The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+/* Request 3: .filter() 
 
-    */
-const lowPopulationAnimals = [];
+The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+
+*/
+const lowPopulationAnimals = zooAnimals.filter(currentValue => {
+    return currentValue.population < 5;
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
